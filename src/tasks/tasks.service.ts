f79c03fb066a89cc152c276d70ead7036d9e6eef
panutat-p <pp.panutat@gmail.com>
2022-03-04 22:16:34 +0700

    POST: create a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { Task } from "./tasks.model";
+import { Task, TaskStatus } from "./tasks.model";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class TasksService {
@@ -8,5 +9,17 @@ export class TasksService {
   getAllTasks(): Task[] {
     // No access modifier, public by default
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
