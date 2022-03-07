a0984b70d179a1f55fd67a3b14bda38948568e91
panutat-p <pp.panutat@gmail.com>
2022-03-07 22:14:42 +0700

    Pipe, validate enum and query string

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,15 @@
 import { TaskStatus } from "../tasks.model";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 
 export class GetTasksFilterDto {
+  // without IsOptional()
+  // request must include filter in the URL
+
+  @IsOptional()
+  @IsEnum(TaskStatus) // invalid status in the request body will get error
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
