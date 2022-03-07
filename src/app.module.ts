761d9d5f7082cd500092c82f0c70e4c9a6d32b52
panutat-p <pp.panutat@gmail.com>
2022-03-07 22:51:02 +0700

    TypeORM, install packages, configs, declare entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TasksController } from "./tasks/tasks.controller";
 import { TasksService } from "./tasks/tasks.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "arjuna.db.elephantsql.com",
+      port: 5432,
+      username: "siqubxqc",
+      password: "", // 👈🏻 fill password here
+      database: "siqubxqc",
+      autoLoadEntities: true,
+      synchronize: true, // auto migration
+    }),
+  ],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
