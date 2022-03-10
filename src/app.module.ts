34d9d3bda16bfe2c5c0ed2cc652157fc359647a8
panutat-p <pp.panutat@gmail.com>
2022-03-11 00:13:45 +0700

    refactor app module to use declaration funciton instead of arrow function

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,16 +16,18 @@ import { configValidationSchema } from "./config.joi-schema";
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get<string>("DB_HOST"),
-        port: configService.get<number>("DB_PORT"),
-        username: configService.get<string>("DB_USERNAME"),
-        password: configService.get<string>("DB_PASSWORD"),
-        database: configService.get<string>("DB_DATABASE"),
-        autoLoadEntities: configService.get<boolean>("DB_AUTO_ENTITY"),
-        synchronize: configService.get<boolean>("DB_SYNC"),
-      }),
+      useFactory: async function (configService: ConfigService) {
+        return {
+          type: "postgres",
+          host: configService.get<string>("DB_HOST"),
+          port: configService.get<number>("DB_PORT"),
+          username: configService.get<string>("DB_USERNAME"),
+          password: configService.get<string>("DB_PASSWORD"),
+          database: configService.get<string>("DB_DATABASE"),
+          autoLoadEntities: configService.get<boolean>("DB_AUTO_ENTITY"),
+          synchronize: configService.get<boolean>("DB_SYNC"),
+        };
+      },
     }),
     TasksModule,
     AuthModule,
