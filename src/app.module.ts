7a1385f8455747f284c8829488a0f91fee4734c6
panutat-p <pp.panutat@gmail.com>
2022-03-10 17:17:55 +0700

    DEV: move DB config to env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,26 @@ import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksModule } from "./tasks/tasks.module";
 import { AuthModule } from "./auth/auth.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "arjuna.db.elephantsql.com",
-      port: 5432,
-      username: "siqubxqc",
-      password: "", // 👈🏻 fill password here
-      database: "siqubxqc",
-      autoLoadEntities: true,
-      synchronize: true, // auto migration
+    ConfigModule.forRoot({
+      envFilePath: [`config/.env.stage.${process.env.STAGE}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        autoLoadEntities: configService.get<boolean>("DB_AUTO_ENTITY"),
+        synchronize: configService.get<boolean>("DB_SYNC"),
+      }),
     }),
     TasksModule,
     AuthModule,
